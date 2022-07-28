d25eb7a55aab84e45fdde8f766cd8847d17921c4
TranMinhQuyet <44421023+caigiha1@users.noreply.github.com>
2022-07-29 00:25:06 +0700

    first commit 🚀

--- index.ts
+++ index.ts
@@ -2,6 +2,22 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+  finished: any;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then((res) => {
-  console.log(res.data);
+  // console.log(res.data);
+  const todo = res.data as Todo;
+
+  const ID = todo.id;
+  const title = todo.title;
+  const finished = todo.completed;
+
+  console.log(
+    `The todo with Id: ${ID} Has a title of: ${title} It is finished? ${finished}`
+  );
 });
